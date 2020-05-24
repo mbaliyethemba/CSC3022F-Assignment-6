a3aadffa9dafd6aa7dbc35af1ca49dd605485da1
Themba <SHNMBA004@uct.ac.za>
2020-05-24 18:53:37 +0200

    Added the state constructor:

--- state.cpp
+++ state.cpp
@@ -1,1 +1,18 @@
+//Mbaliyethemba Shangase
+//SHNMBA004
+//Assignment 6
+//29 May 2020
 
+#include <vector>
+#include <map>
+#include <iostream>
+#include "state.h"
+
+state::state(std::vector<char> action, std::map<int,int> reward, int val, int state_number, float discount, float probability){
+	this->action = action;
+	this->reward = reward;
+	this->val = val;
+	this->state_number = state_number;
+	this->discount = discount;
+	this->probability = probability;
+}
