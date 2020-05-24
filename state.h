80d3f0bca361c92e1e8174de61bae6b24fc661a5
Themba <SHNMBA004@uct.ac.za>
2020-05-24 18:40:12 +0200

    Added the state constructor

--- state.h
+++ state.h
@@ -1,1 +1,24 @@
+//Mbaliyethemba Shangase
+//SHNMBA004
+//Assignment 6
+//29 May 2020
 
+#include <vector>
+#include <map>
+#include <iostream>
+
+#ifndef STATE_H
+#define STATE_H
+
+class state{
+	private:
+		std::vector<char> action;
+		std::map<int, int> reward;
+		int state_number;
+		float discount, probability, val;
+	
+	public:
+		state();
+}
+
+#endif
