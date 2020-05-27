dc134aeb168916ef2a754de2f342378cf49701ea
Themba <SHNMBA004@uct.ac.za>
2020-05-27 23:24:00 +0200

    Added the default constructor class

--- grid.h
+++ grid.h
@@ -1,1 +1,21 @@
+//Mbaliyethemba Shangase
+//SHNMBA004
+//Assignment 6
+//29 May 2020
 
+#include <vector>
+#include <map>
+#include "state.h"
+#include <string>
+
+#ifndef GRID_H
+#define GRID_H
+
+class grid{
+	private:
+		std::vector<std::vector<state>> world;
+	
+	public:
+		grid();
+};
+#endif
