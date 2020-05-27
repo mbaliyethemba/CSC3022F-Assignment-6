5419cfd749ab1247e4912d748985850c35f09510
Themba <SHNMBA004@uct.ac.za>
2020-05-27 23:45:58 +0200

    Added the function to converge grid

--- grid.cpp
+++ grid.cpp
@@ -21,6 +21,18 @@ grid::grid(std::vector<std::vector<state>> v){
 	this->world = v;
 }
 
+//converging the grid
+bool grid::converge(grid g){
+	for(size_t col = 0; col < this->world.size(); col++){
+		for(size_t row = 0; row < this->world[col].size(); row++){
+			if(this->world[col][row].get_value() != g.world[col][row].get_value()){
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
 //filling the grid for working
 void grid::fill(){
 	grid g, a;
