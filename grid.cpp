89841819fdb199f78bc4d2fe73fdd921d3f72e26
Themba <SHNMBA004@uct.ac.za>
2020-05-27 23:26:07 +0200

    Added the default constructor class

--- grid.cpp
+++ grid.cpp
@@ -1,1 +1,13 @@
+//Mbaliyethemba Shangase
+//SHNMBA004
+//Assignment 6
+//29 May 2020
 
+#include "grid.h"
+#include <algorithm>    // std::max
+#include <fstream>
+#include "float.h"
+
+//default constructor
+grid::grid(){
+}
